ea6e7fae18cbaa402636e66ae25e1dd162152883
Mikhail M <mme.1994@yandex.ru>
2017-11-20 11:46:23 +0300

    Add components Yii2

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'VJKILd7Dch-_S-n07spT1Y5uVTxmQ2fR',
+        ],
+    ],
+];
---
+++ frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'QfjO9VfaNAIkjuD3X0EsyK6Gad_qIW6w',
+        ],
+    ],
+];
